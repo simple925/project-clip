7f25eedcc26da84a4e2f11865a1b3c85f46161e0
DianaPaik <dianapaik919@gmail.com>
2024-04-07 14:30:22 +0900

    [글쓰기] 휴가계 부분 ui 완료(소진 휴가일수 자동 계산 및 datepicker 휴가 사용일 input에 출력)

--- src/app/Write/page.tsx
+++ src/app/Write/page.tsx
@@ -13,9 +13,9 @@ import {
   Textarea,
   ButtonProps,
   Button,
-  Tabs,
-  rem,
   NumberInput,
+  PillsInput,
+  Pill,
 } from "@mantine/core";
 import style from "./Write.module.css";
 import dayjs from "dayjs";
@@ -43,9 +43,10 @@ const user = {
   userUUID: 1234,
   userName: "백지연",
   userPhoneNm: "01064861568",
+  userDepartment: "서비스&기획부",
   userGrade: "사원",
   totalVacation: 15,
-  usedVacation: 3
+  usedVacation: 3,
 };
 
 // 글쓰기 탭 정보
@@ -55,28 +56,80 @@ const writeTabBtnItems = [
   { label: "지출결의서", value: "지출결의서" },
 ];
 
-
 /** 글쓰기 페이지 출력부  */
 // export default function으로 외부 출력
 export default function WritePage() {
-
-  // #### 휴가계 변수 선언
+  // ################ Define Variables
+  // 휴가계 변수 선언
   const [vacationTitle, setVacationTitle] = useState(""); // 휴가계 제목
   const [datesValue, setDatesValue] = useState<Date[]>([]); // 신청하는 날짜(배열 가능)
+  const [formattedApplicatedDates, setFormattedApplicatedDates] = useState<
+    string[]
+  >([]); // 변환된 날짜들을 저장할 배열
   const [writtenDate, setWrittenDate] = useState<Date | null>(new Date()); // 휴가계 작성일
   const formatttedDate = dayjs(writtenDate).format("YYYY-MM-DD");
   const [content, setContent] = useState(""); // 휴가 사유
   const [lastUsedVacation, setLastUsedVacation] = useState(user.usedVacation); // 소진 휴가일수
+
+  // 현재 선택된 datesValue의 길이에 따라 사용된 휴가일수를 계산
+  const calculateUsedVacation = () => {
+    const selectedDatesLength = datesValue.length;
+    // 선택된 날짜의 수를 사용된 휴가일수로 설정 (최대 5일)
+    const usedVacationDays = Math.min(selectedDatesLength, 5);
+    return usedVacationDays;
+  };
+
+  // 선택된 날짜를 string으로 변환하여 배열에 추가하는 함수
+  const updateFormattedApplicatedDates = () => {
+    const formattedDates = datesValue.map((date) =>
+      dayjs(date).format("YYYY-MM-DD")
+    );
+    // 날짜를 오름차순으로 정렬
+    formattedDates.sort(
+      (a, b) => new Date(a).getTime() - new Date(b).getTime()
+    );
+    setFormattedApplicatedDates(formattedDates);
+  };
+
   // 휴가계 제목을 자동으로 업데이트하는 함수
   const updateVacationTitle = () => {
     setVacationTitle(
       `[${user.userName} ${user.userGrade}] 휴가 신청 (${formatttedDate})`
     );
   };
-  
+
+  // 신청일이 변경될 때마다 휴가게 제목 자동 변경
   useEffect(() => {
     updateVacationTitle();
   }, [writtenDate]);
+
+  // datesValue가 변경될 때마다 formattedApplicatedDates를 업데이트
+  // 해당 내용 휴가 사용일에 pill으로 출력됩니다
+  useEffect(() => {
+    // 1. input 화면 출력을 위한 업데이트. string[]으로 출력
+    updateFormattedApplicatedDates();
+    // 2. 현재 선택된 날짜로 인한 휴가 소진일수 계산
+    const usedVacationDays = calculateUsedVacation();
+    setLastUsedVacation(usedVacationDays);
+  }, [datesValue]);
+
+  // ################ Define Variables
+  // 지출결의서 변수 선언
+  // const [disbursementTitle, setDisbursementTitle] = useState(""); // 지출결의서 제목
+  // const [disbursementWrittenDate, setDisbursementWrittenDate] =
+  //   useState<Date | null>(new Date()); // 지급 요청일
+  // const formatttedDisbursementDate = dayjs(disbursementWrittenDate).format(
+  //   "YYYY-MM-DD"
+  // );
+  // const [content, setContent] = useState(""); // 일금
+  // const [lastUsedVacation, setLastUsedVacation] = useState(user.usedVacation); // 지급 방법
+  // const [disbursementContent, setDisbursementContent] = useState("");  // 지출 세부 내역(무엇을 구매했는지?)
+  // const [disbursementBank, setDisbursementBank] = useState(""); // 은행 이체 명세(은행명, 수취인, 계좌번호)
+  // const [] // 작성일자
+  // const [] // 공급가액
+  // const [] // 부가세액
+  // const [] // 합계
+  // const // 증빙 
 
   return (
     <div className={style.container}>
@@ -100,17 +153,13 @@ export default function WritePage() {
             minRows={20}
           /> */}
           {/* ############# 휴가계 입력 폼 */}
-          <Input.Wrapper label="휴가계 제목" withAsterisk error="자동 완성됨">
+          <Input.Wrapper label="휴가계 제목" withAsterisk>
             <Input readOnly value={vacationTitle} placeholder={vacationTitle} />
           </Input.Wrapper>
           <Input.Wrapper label="신청자" withAsterisk>
             <Input variant="filled" readOnly value={user.userName} />
           </Input.Wrapper>
-          <Input.Wrapper
-            label="신청일"
-            withAsterisk
-            error="자동 완성됨, 수정 가능"
-          >
+          <Input.Wrapper label="신청일" withAsterisk>
             <MantineDatePickerInput
               locale="ko"
               value={writtenDate}
@@ -118,7 +167,14 @@ export default function WritePage() {
             />
             {/* 입력란에서 포커스가 떠날 때 휴가계 제목 업데이트 */}
           </Input.Wrapper>
-          <Input.Wrapper label="휴가 사용일" withAsterisk error="">
+          <Input.Wrapper
+            label="휴가 사용일"
+            withAsterisk
+            error={
+              formattedApplicatedDates.length === 0 &&
+              "휴가 사용일을 선택해주세요."
+            }
+          >
             <MantineDatePicker
               size="md"
               locale="ko"
@@ -126,22 +182,38 @@ export default function WritePage() {
               value={datesValue}
               onChange={setDatesValue}
             />
-          </Input.Wrapper>
-          <Input.Wrapper label="휴가 사유" withAsterisk error="자동 완성됨">
-            <Input placeholder="제목을 입력하세요." />
+            {/* 휴가 사용일 선택시 해당 내용이 pillInput으로 자동으로 추가됨 */}
+            {/* datepicker 날짜 5개로 제한하는 속성있는지 확인 필요 */}
+            <PillsInput>
+              <Pill.Group>
+                {formattedApplicatedDates.map((date, index) => (
+                  <Pill key={index}>{date}</Pill>
+                ))}
+              </Pill.Group>
+            </PillsInput>
           </Input.Wrapper>
           <Input.Wrapper
-            label="소진 휴가일수 / 연차일수"
+            label="휴가 사유"
             withAsterisk
-            error="자동 완성됨"
+            error={content.trim() == "" && "휴가 사유를 입력해주세요."}
           >
+            <Input
+              value={content}
+              onChange={(event) => setContent(event.currentTarget.value)}
+              placeholder="휴가 사유를 입력하세요."
+            />
+          </Input.Wrapper>
+          <Input.Wrapper label="소진 휴가일수 / 연차일수" withAsterisk>
             <NumberInput
               className={style["input_number_box"]}
-              value={user.usedVacation}
+              value={user.usedVacation + lastUsedVacation}
             />
             / {user.totalVacation}
           </Input.Wrapper>
           {/* ############# 지출결의서 입력 폼 */}
+          {/* <Input.Wrapper label="지출결의서 제목" withAsterisk>
+            <Input readOnly value={vacationTitle} placeholder={vacationTitle} />
+          </Input.Wrapper> */}
         </div>
       </Paper>
       {/* 버튼 2개를 기능과 label을 받아 뿌려주게끔 추후 수정 - */}
