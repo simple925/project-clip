d4194d87694af1333d604bda7290232059b8a36a
simple925 <60890584+simple925@users.noreply.github.com>
2024-05-02 17:01:18 +0900

    대쉬보드 추가 css , page

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,8 +4,8 @@ import { MantineProvider, ColorSchemeScript } from '@mantine/core';
 import { theme } from '@/theme';
 
 export const metadata = {
-  title: 'Mantine Next.js template',
-  description: 'I am using Mantine with Next.js!',
+  title: 'clip',
+  description: '모두 도망쳐',
 };
 
 export default function RootLayout({ children }: { children: any }) {
