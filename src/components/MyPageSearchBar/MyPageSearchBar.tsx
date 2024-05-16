be3a4914312f0d866fecd9d7889820f43e53569b
SaebomSon <newspring56@gmail.com>
2024-05-16 19:53:12 +0900

    [마이페이지] 검색바 event 추가

--- src/components/MyPageSearchBar/MyPageSearchBar.tsx
+++ src/components/MyPageSearchBar/MyPageSearchBar.tsx
@@ -15,34 +15,41 @@ export function MyPageSearchBar() {
 
   return (
     <Stack w={800} mt={40} align="flex-end">
-        <TextInput
-          radius="xl"
-          size="sm"
-          style={{ width: rem(300)}}
-          placeholder="Search questions"
-          value={values}
-          onChange={(event) => {
-            event.preventDefault();
-            setValues(event.currentTarget.value);
-          }}
-          rightSectionWidth={42}
-          leftSection={
-            <IconSearch style={{ width: rem(18), height: rem(18) }} stroke={1.5} />
-          }
-          rightSection={
-            <ActionIcon
-              size={32}
-              radius="xl"
-              color={theme.primaryColor}
-              variant="filled"
-            >
-              <IconArrowRight
-                style={{ width: rem(18), height: rem(18) }}
-                stroke={1.5}
-              />
-            </ActionIcon>
-          }
-        />
+      <TextInput
+        radius="xl"
+        size="sm"
+        style={{ width: rem(300) }}
+        placeholder="Search questions"
+        value={values}
+        onChange={(event) => {
+          setValues(event.currentTarget.value);
+        }}
+        rightSectionWidth={42}
+        leftSection={
+          <IconSearch
+            style={{ width: rem(18), height: rem(18) }}
+            stroke={1.5}
+          />
+        }
+        rightSection={
+          <ActionIcon
+            size={32}
+            radius="xl"
+            color={theme.primaryColor}
+            variant="filled"
+            onClick={(event) => {
+              event.preventDefault();
+              // TODO : 검색 쿼리에 values 넘겨줘야함
+              // console.log(values)
+            }}
+          >
+            <IconArrowRight
+              style={{ width: rem(18), height: rem(18) }}
+              stroke={1.5}
+            />
+          </ActionIcon>
+        }
+      />
     </Stack>
   );
 }
