95f9a4381a90f2739d0f74186d0add9b8f8533dd
DianaPaik <dianapaik919@gmail.com>
2024-05-30 21:07:55 +0900

    [내 그룹]

--- src/components/MainGroup/MainGroup.tsx
+++ src/components/MainGroup/MainGroup.tsx
@@ -1,22 +1,56 @@
 "use client";
 
-import { Accordion } from "@mantine/core";
+import { Accordion, ColorSwatch, CheckIcon, rem } from "@mantine/core";
 import styles from "./MainGroup.module.css";
-
-const calendarItems = [
-  {
-    value: "내 캘린더",
-    description: "생일",
-  },
-];
+import { useState } from "react";
 
 export function MainGroup() {
-  const items = calendarItems.map((item) => (
-    <Accordion.Item key={item.value} value={item.value}>
-      <Accordion.Control>{item.value}</Accordion.Control>
-      <Accordion.Panel>{item.description}</Accordion.Panel>
-    </Accordion.Item>
-  ));
+
+  const [obj, setObj] = useState([
+    { key: 1, value: "생일", description: "생일", color: "red" },
+    { key: 2, value: "여름 휴가", description: "여름 휴가", color: "blue" },
+    { key: 3, value: "프로젝트 회의", description: "프로젝트 회의", color: "green" },
+  ]);
+  const [selectedEvent, setSelectedEvent] = useState(
+    { key: 1, value: "생일", description: "생일", color: "red" }
+  );
+
+  const [checkedColor, setCheckedColor] = useState(true); // 선택
+  const [selectedColor, setSelectedColor] = useState(null);
+
+  function onSelectedColorBtn(item: any) {
+    setCheckedColor((c) => !c)
+    setSelectedColor(item.color);
+    setSelectedEvent(item)
+    console.log('###현재 선택 이벤트 :', item)
+  }
+
+  // function onSelectedEvent(value: any) {
+  //   setSelectedEvent(value)
+  //   console.log('###현재 선택 이벤트 :', value)
+  // }
+
+  const items = obj.map((item) => {
+    const isActive = item.color === selectedColor;
+    const colorBtn = isActive ? '#fff' : 'none';
+    const cursorBtn = isActive ? 'pointer' : 'default';
+
+    const colors = (
+      <ColorSwatch
+        component="button"
+        color={item.color}
+        style={{ color: colorBtn, cursor: cursorBtn }}
+      >
+        {checkedColor && <CheckIcon style={{ width: rem(12), height: rem(12) }} />}
+      </ColorSwatch>
+    );
+    return (
+      <Accordion.Item key={item.value} value={item.value}>
+        <Accordion.Control onClick={() => onSelectedColorBtn(item)}>{colors}{item.value}</Accordion.Control>
+        <Accordion.Panel>{item.color}</Accordion.Panel>
+      </Accordion.Item>
+    );
+  });
 
   return (
     <div className={styles.MainGroup}>
