ca55c790dcbccad8936834563b2878be8711e559
DianaPaik <dianapaik919@gmail.com>
2024-06-04 16:55:55 +0900

    [메인 달력] 툴바 스타일링(화살표 아이콘 처리)

--- src/components/CalendarToolbar/CalendarToolbar.tsx
+++ src/components/CalendarToolbar/CalendarToolbar.tsx
@@ -1,29 +1,32 @@
 'use client';
 import React, { useState } from "react";
+import { Button } from '@mantine/core';
+import style from './CalendarToolbar.module.css';
+import { IconArrowRight, IconArrowLeft } from '@tabler/icons-react';
+import { rem } from "@mantine/core";
 
 // 이번달, 이전 <-> 다음 월별 이동 툴바
-// TODO 추가 스타일링 필요
+// TODO 추가 스타일링 필요 ==> mantine 버튼 스타일 참고하여 수정
+
 export function CalendarToolbar({ date, onNavigate }:any) {
   const navigate = (action:any) => {
     onNavigate(action);
   };
   
   return (
-    <div className="rbc-toolbar">
-      <span className="rbc-btn-group">
-        <button type="button" onClick={() => navigate('TODAY')}>
-          이번달
-        </button>
-        <button type="button" onClick={() => navigate('PREV')}>
-          이전
-        </button>
-        <span className="rbc-toolbar-label">
+    <div className={style.toolbar}>
+    <Button.Group >
+      <Button variant="default" onClick={() => navigate('TODAY')}> 이번달</Button>
+      <Button variant="default" onClick={() => navigate('PREV')}>
+          <IconArrowLeft />
+      </Button>
+        <span className={style.toolbarLabel}>
           {`${date.getFullYear()}년 ${date.getMonth() + 1}월`}
         </span>
-        <button type="button" onClick={() => navigate('NEXT')}>
-          다음
-        </button>
-      </span>
+      <Button variant="default" onClick={() => navigate('NEXT')}>
+          <IconArrowRight />
+      </Button >
+    </Button.Group>
     </div>
   );
 }
