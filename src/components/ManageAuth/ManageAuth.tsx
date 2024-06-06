4183f5e0cf0ff5792e87c3ae0d55d2ae6b249b8e
SaebomSon <newspring56@gmail.com>
2024-06-06 21:45:31 +0900

    [권한관리] 권한대상 추가 autocomplete으로 변경

--- src/components/ManageAuth/ManageAuth.tsx
+++ src/components/ManageAuth/ManageAuth.tsx
@@ -6,7 +6,6 @@ import {
   Text,
   Container,
   Grid,
-  Input,
   ActionIcon,
   Checkbox,
   ScrollArea,
@@ -17,8 +16,10 @@ import {
   Group,
   Divider,
   CloseButton,
-  UnstyledButton,
   Modal,
+  AutocompleteProps,
+  Avatar,
+  Autocomplete,
 } from "@mantine/core";
 import classes from "./ManageAuth.module.css";
 import { IconRefresh, IconUserCheck, IconUserPlus } from "@tabler/icons-react";
@@ -32,10 +33,51 @@ import { IconTrash } from "@tabler/icons-react";
  * 1. 권한 대상 클릭 시 대상이 갖고 있는 권한들의 체크 박스 활성화하는 로직 추가 필요
  * 2. 저장 클릭 시 DB Insert 또는 Update
  * 3. 초기화 버튼 추가 필요 - 로직 추가해야함
- * 4. 권한 대상 삭제 버튼 필요(삭제 후 grid 자체를 지워야함 + 연속으로 삭제 가능하도록 해야함)
  */
 
 export function ManageAuth() {
+  /* 사용자 정보 */
+  const usersData: Record<string, { image: string; email: string }> = {
+    "Emily Johnson": {
+      image:
+        "https://raw.githubusercontent.com/mantinedev/mantine/master/.demo/avatars/avatar-7.png",
+      email: "emily92@gmail.com",
+    },
+    "Ava Rodriguez": {
+      image:
+        "https://raw.githubusercontent.com/mantinedev/mantine/master/.demo/avatars/avatar-8.png",
+      email: "ava_rose@gmail.com",
+    },
+    "Olivia Chen": {
+      image:
+        "https://raw.githubusercontent.com/mantinedev/mantine/master/.demo/avatars/avatar-4.png",
+      email: "livvy_globe@gmail.com",
+    },
+    "Ethan Barnes": {
+      image:
+        "https://raw.githubusercontent.com/mantinedev/mantine/master/.demo/avatars/avatar-1.png",
+      email: "ethan_explorer@gmail.com",
+    },
+    "Mason Taylor": {
+      image:
+        "https://raw.githubusercontent.com/mantinedev/mantine/master/.demo/avatars/avatar-2.png",
+      email: "mason_musician@gmail.com",
+    },
+  };
+  const renderAutocompleteOption: AutocompleteProps["renderOption"] = ({
+    option,
+  }) => (
+    <Group gap="sm">
+      <Avatar src={usersData[option.value].image} size={36} radius="xl" />
+      <div>
+        <Text size="sm">{option.value}</Text>
+        <Text size="xs" opacity={0.5}>
+          {usersData[option.value].email}
+        </Text>
+      </div>
+    </Group>
+  );
+
   /* 대상 입력 값 */
   const [input, setInput] = useState({
     name: "",
@@ -49,27 +91,13 @@ export function ManageAuth() {
     } else {
       const newInput = { name: props.name, uuid: props.uuid };
       setObj([newInput, ...obj]);
-      setInput({
-        name: "",
-        uuid: "",
-      });
+      setInput({ name: "", uuid: "" });
     }
   }
 
   /* 권한 대상 삭제 */
-  function removeObject(props: any) {
-    console.log(props);
-    const newObjList = [
-      {
-        name: input.name,
-        uuid: input.uuid,
-      },
-    ];
-    obj.forEach((item) => {
-      if (item.uuid !== props.uuid) {
-        newObjList.push(item);
-      }
-    });
+  function removeObject(uuid: String) {
+    const newObjList = obj.filter((item) => item.uuid !== uuid);
     setObj(newObjList);
   }
 
@@ -112,7 +140,7 @@ export function ManageAuth() {
     >
       <Grid.Col span={9}>{item.name}</Grid.Col>
       <Grid.Col span={1}>
-        <CloseButton size={20} onClick={() => removeObject(item)} />
+        <CloseButton size={20} onClick={() => removeObject(item.uuid)} />
       </Grid.Col>
     </Grid>
   ));
@@ -151,7 +179,6 @@ export function ManageAuth() {
     </Table.Tr>
   ));
 
-
   /* 권한관리 modal */
   const [opened, { open, close }] = useDisclosure(false);
   const modalRows = values.map((element) => (
@@ -167,12 +194,18 @@ export function ManageAuth() {
       <Table.Td colSpan={3}>~~권한설명 추가~~</Table.Td>
       <Table.Td>
         <ActionIcon variant="outline" color="indigo">
-          <IconPencil style={{ width: '70%', height: '70%' }} stroke={1.5}></IconPencil>
+          <IconPencil
+            style={{ width: "70%", height: "70%" }}
+            stroke={1.5}
+          ></IconPencil>
         </ActionIcon>
       </Table.Td>
       <Table.Td>
         <ActionIcon variant="outline" color="red">
-          <IconTrash style={{ width: '70%', height: '70%' }} stroke={1.5}></IconTrash>
+          <IconTrash
+            style={{ width: "70%", height: "70%" }}
+            stroke={1.5}
+          ></IconTrash>
         </ActionIcon>
       </Table.Td>
     </Table.Tr>
@@ -186,7 +219,8 @@ export function ManageAuth() {
         </Title>
         <Flex></Flex>
         <Button variant="light" color="indigo" size="xs">
-          <IconRefresh style={{ width: '70%', height: '70%' }} stroke={1.5} />초기화
+          <IconRefresh style={{ width: "70%", height: "70%" }} stroke={1.5} />
+          초기화
         </Button>
         <Grid style={{ width: "100%", height: "40%" }} justify="center">
           <Grid.Col span={4}>
@@ -224,21 +258,33 @@ export function ManageAuth() {
                 <Text size="sm" mr={20}>
                   권한 추가
                 </Text>
-                <Input
+                <Autocomplete
+                  data={[
+                    "Emily Johnson",
+                    "Ava Rodriguez",
+                    "Olivia Chen",
+                    "Ethan Barnes",
+                    "Mason Taylor",
+                  ]}
+                  renderOption={renderAutocompleteOption}
                   size="xs"
-                  w="250"
-                  placeholder="대상을 입력하세요."
+                  w={250}
+                  maxDropdownHeight={300}
+                  placeholder="Search for employee"
                   value={input.name}
-                  onChange={(event) => {
-                    event.preventDefault();
+                  onChange={(name) => {
                     setInput({
-                      name: event.currentTarget.value,
-                      uuid: "#plus",
-                      // link --> 사용자uuid로 추후 변경 필요
+                      name: name,
+                      uuid: "aaaa",
                     });
                   }}
                 />
-                <ActionIcon variant="filled" onClick={() => plusObject(input)}>
+                <ActionIcon
+                  variant="filled"
+                  onClick={() => {
+                    plusObject(input);
+                  }}
+                >
                   <IconUserPlus style={{ width: "60%", height: "60%" }} />
                 </ActionIcon>
               </Group>
@@ -264,8 +310,6 @@ export function ManageAuth() {
                               allChecked
                                 ? setSelectedRows([])
                                 : setSelectedRows(check.map((v) => v.key));
-                              // onChange() 종료 이후에 allChecked 값이 변경됨
-                              // console.log("allChecked >> ", allChecked)
                               return check;
                             })
                           }
@@ -277,8 +321,19 @@ export function ManageAuth() {
                   <Table.Tbody>{rows}</Table.Tbody>
                 </Table>
               </ScrollArea>
-              <Button variant="light" color="red" size="xs" mt={7} justify="Flex-end" onClick={open}>
-                <IconUserCheck style={{ width: '70%', height: '70%' }} stroke={1.5} />권한 관리
+              <Button
+                variant="light"
+                color="red"
+                size="xs"
+                mt={7}
+                justify="Flex-end"
+                onClick={open}
+              >
+                <IconUserCheck
+                  style={{ width: "70%", height: "70%" }}
+                  stroke={1.5}
+                />
+                권한 관리
               </Button>
             </Paper>
           </Grid.Col>
@@ -305,12 +360,14 @@ export function ManageAuth() {
               </Table.Thead>
               <Table.Tbody>{modalRows}</Table.Tbody>
             </Table>
-            <Flex mih={50}
+            <Flex
+              mih={50}
               gap="md"
               justify="center"
               align="center"
               direction="row"
-              wrap="wrap">
+              wrap="wrap"
+            >
               <Button variant="filled" radius="xl">
                 저장
               </Button>
