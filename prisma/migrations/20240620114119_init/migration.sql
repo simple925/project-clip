b2f396d06507293646a9fb81aefb8bb7e5d7758a
DianaPaik <dianapaik919@gmail.com>
2024-06-24 12:56:55 +0900

    json-server-generator 셋팅

---
+++ prisma/migrations/20240620114119_init/migration.sql
@@ -0,0 +1,7 @@
+-- CreateTable
+CREATE TABLE "User" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "userUUID" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "username" TEXT NOT NULL
+);
